182bd9db3cbdfd519b542b1ce9d561bcb5fb27e7
VladSurzhko <surzhkovlad69@gmail.com>
2023-06-28 12:25:56 +0200

    upi

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -15,7 +15,7 @@ export const HeaderStyled = styled.header`
   padding-top: 12px;
   padding-bottom: 12px;
   color: #fff;
-  background-color: #483d8b;
+  background-color: #00d4ff;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
@@ -25,17 +25,19 @@ export const NavList = styled.nav`
   align-items: start;
   gap: 20px;
   list-style: none;
+  
 `;
 
  export const StyledNavLink = styled(NavLink)`
  font-weight: 500;
  color: white;
  transition: all 250ms linear;
+ list-style: none;
 
  &.active,
  &:hover,
  &:focus {
-   color: #bc8f8f;
+   color: black;
  }
 
 `;
