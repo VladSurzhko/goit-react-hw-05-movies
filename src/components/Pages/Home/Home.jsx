c3b94cd2b5c3ec9c45de9eae8699f1e4fc50d903
VladSurzhko <surzhkovlad69@gmail.com>
2023-06-29 13:26:20 +0200

    2

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 // import axios from 'axios';
 import { getMovies } from "../../Api/Api"
 import { Loader } from 'components/Loader/Loader';
-import { ListTopMovies, MoviesText, SectionTopMovies, Links } from './Home.styled';
+import { ListTopMovies, MoviesText, SectionTopMovies, } from './Home.styled';
 import { Link } from 'react-router-dom';
 
 
