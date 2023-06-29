0c7e6999470c21ca03936def02d00a5bcf2c481a
VladSurzhko <surzhkovlad69@gmail.com>
2023-06-29 21:00:01 +0200

    5

--- src/components/Pages/Movies/Movies.jsx
+++ src/components/Pages/Movies/Movies.jsx
@@ -1,97 +1,10 @@
-// import { useEffect, useState } from 'react';
-// import { Link, useSearchParams, useLocation } from 'react-router-dom';
-// import { getMovie } from 'components/Api/Api';
-// import { Loader } from 'components/Loader/Loader';
-// import {
-//   MovieList,
-//   MovieItem,
-//   MoviesWrapper,
-//   SearchForm,
-//   SearchLabel,
-//   SearchInput,
-//   NotFoundMessage,
-//   NotFoundContainer,
-// } from './Movies.styled';
-
-// const Movies = () => {
-//   const [searchMovie, setSearchMovie] = useSearchParams();
-//   const [movies, setMovie] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [isInputEmpty, setIsInputEmpty] = useState(true);
-//   const movieName = searchMovie.get('movieName') ?? '';
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     const fetchMovie = async () => {
-//       try {
-//         setIsLoading(true);
-//         const data = await getMovie(movieName);
-//         setMovie(data.results);
-//         if (data.results.length === 0) {
-//           setError(`Movie '${movieName}' not found`);
-//         } else {
-//           setError(null);
-//         }
-//       } catch (error) {
-//         setError(error.message);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-
-//     if (!isInputEmpty) {
-//       fetchMovie();
-//     }
-//   }, [isInputEmpty, movieName]);
-
-//   const handleSearch = evt => {
-//     const movieNameValue = evt.target.value;
-//     if (movieNameValue === '') {
-//       return setSearchMovie({});
-//     }
-//     setSearchMovie({ movieName: movieNameValue });
-//     setIsInputEmpty(false);
-//   };
-
-//   return (
-//     <MoviesWrapper>
-//       {isLoading && <Loader />}
-
-//       <SearchForm>
-//         <SearchLabel>Movie search</SearchLabel>
-//         <SearchInput type="text" value={movieName} onChange={handleSearch} />
-//         {!isInputEmpty && error && (
-//           <NotFoundContainer>
-//             <NotFoundMessage>Movie {movieName} not found</NotFoundMessage>
-//           </NotFoundContainer>
-//         )}
-//       </SearchForm>
-
-//       <MovieList>
-//         {movies.map(movie => (
-//           <MovieItem key={movie.id}>
-//             <Link to={`${movie.id}`} state={{ from: location }}>
-//               {movie.title}
-//             </Link>
-//           </MovieItem>
-//         ))}
-//       </MovieList>
-//     </MoviesWrapper>
-//   );
-// };
-
-// export default Movies;
-
-
-
-
 
 
 import { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import { getMovie } from 'components/Api/Api';
 import { Loader } from 'components/Loader/Loader';
+import { FormSearch, InputSearch, LabelSearch, SearchButton } from './Movies.styled';
 
 const Movies = () => {
   const [searchMovie, setSearchMovie] = useSearchParams();
@@ -99,22 +12,31 @@ const Movies = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const movieName = searchMovie.get('movieName') ?? '';
+  const [isInputEmpty, setIsInputEmpty] = useState(true);
+
 
   useEffect(() => {
-    const fetchMovie = async () => {
-      try {
-        setIsLoading(true);
-        const data = await getMovie(movieName);
-        setMovies(data.results);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchMovie();
-  }, [movieName]);
+        const fetchMovie = async () => {
+          try {
+            setIsLoading(true);
+            const data = await getMovie(movieName);
+            setMovies(data.results);
+            if (data.results.length === 0) {
+              setError(`Movie '${movieName}' not found`);
+            } else {
+              setError(null);
+            }
+          } catch (error) {
+            setError(error.message);
+          } finally {
+            setIsLoading(false);
+          }
+        };
+    
+        if (!isInputEmpty) {
+          fetchMovie();
+        }
+      }, [isInputEmpty, movieName]);
 
   const handleSearch = evt => {
     const movieNameValue = evt.target.value;
@@ -122,19 +44,26 @@ const Movies = () => {
       return setSearchMovie({});
     }
     setSearchMovie({ movieName: movieNameValue });
+    setIsInputEmpty(false);
   };
 
   return (
     <>
-      {error && <p>Movie {movieName} not found</p>}
+      {!isInputEmpty && error && (
+       <p>Movie {movieName} not found</p>
+      )}
+
       {isLoading && <Loader />}
-      <form>
-        <input 
+      <FormSearch>
+      <LabelSearch>Movie search</LabelSearch>
+        <InputSearch 
         type="text" 
         value={movieName}
         onChange={handleSearch} />
-        <label>Movie search</label>
-      </form>
+        
+      </FormSearch>
+
+      <SearchButton type="submit">Search</SearchButton>
 
       {movies.map(movie => (
         <li key={movie.id}>
--- src/components/Pages/Movies/Movies.styled.jsx
+++ src/components/Pages/Movies/Movies.styled.jsx
@@ -1,62 +1,44 @@
-import styled from 'styled-components';
+import styled from "@emotion/styled"
 
-export const MoviesWrapper = styled.div`
-  display: flex;
-`;
 
-export const SearchForm = styled.form`
-  margin-right: 50px;
-`;
-
-export const NotFoundContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const NotFoundMessage = styled.p`
-  color: red;
-  font-weight: bold;
-`;
-
-export const SearchLabel = styled.label`
-  display: block;
+export const LabelSearch = styled.label`
+margin-top: 20px;
+margin-left: 20px;
+display: block;
   font-weight: bold;
   margin-bottom: 10px;
-`;
+`
 
-export const SearchInput = styled.input`
-  padding: 5px;
+export const InputSearch = styled.input`
+padding: 5px;
+  padding-left: 24px;
   border: 1px solid #ccc;
   border-radius: 4px;
   font-size: 14px;
-`;
+`
 
-export const MovieList = styled.ul`
-  list-style: none;
-  width: 100%;
-  padding: 0;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-gap: 20px;
-  & a{
-    display: block;
-    text-decoration: none;
-    color: grey;
-    text-align: center
-    
-  }
-`;
+export const FormSearch = styled.form`
+margin-top: 20px;
+`
 
-export const MovieItem = styled.li`
-  background-color: #fff;
-  padding: 10px;
+export const SearchButton = styled.button`
+  margin: 12px;
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #00d4ff;
+  color: white;
+  font-weight: bold;
+  border: none;
   border-radius: 4px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  &:hover{
-    background-color: orange;
-    & a{
-      color: black;
-    }
+  cursor: pointer;
+
+  &:hover {
+    color: black;
+    background-color: #b7b7bb;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px black;
   }
 `;
