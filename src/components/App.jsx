b197b7fa2779b7c5e2e3e750d1e4286207317f8b
VladSurzhko <surzhkovlad69@gmail.com>
2023-07-02 13:50:39 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy } from 'react'; 
 import { Layout } from './Layout/Layout';
 import { Reviews } from './Reviews/Reviews';
@@ -27,6 +27,7 @@ export const App = () => {
        <Route path='cast' element={<Cast />} />
        <Route path="reviews" element={<Reviews />} />
      </Route>
+     <Route path="*" element={<Navigate to="/" />} />
    </Route>
   </Routes>
   </div>
--- src/components/Pages/MoviesDetails/MovieDetails.jsx
+++ src/components/Pages/MoviesDetails/MovieDetails.jsx
@@ -1,16 +1,10 @@
-
-
-
-
-import { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getDetailMovie } from 'components/Api/Api';
 import { Loader } from 'components/Loader/Loader';
-import { Outlet, useParams, Link, useLocation } from 'react-router-dom';
+import { Outlet, useParams, Link, useLocation, useNavigate } from 'react-router-dom';
 import { ButtonBack } from './MoviesDetails.styled';
 
 const MovieDetails = () => {
-  const location = useLocation();
-  const goBack = useRef(location.state?.from ?? '/');
   const { movieId } = useParams();
   const [movie, setMovie] = useState([]);
   const [genres, setGenres] = useState([]);
@@ -19,51 +13,47 @@ const MovieDetails = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   
-  
+  const navigate = useNavigate();
+  const location = useLocation();
+  const goBack = location.state?.from || '/';
 
   useEffect(() => {
-    const fetchDetalsMovie = async () => {
+    const fetchDetailsMovie = async () => {
       try {
-        setIsLoading(true)
+        setIsLoading(true);
         const movie = await getDetailMovie(movieId);
         const genres = movie.genres;
         const date = movie.release_date.split('-')[0];
         const url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
         if (movie === []) {
-          setError(`Інформація по фільму відсутня`);
+          setError('Інформація по фільму відсутня');
         } else {
           setGenres(genres);
           setMovie(movie);
           setDate(date);
           setUrlImage(url);
         }
-        // setGenres(genres);
-        // setMovie(movie);
-        // setDate(date);
-        // setUrlImage(url);
       } catch (error) {
         setError(error.message);
       } finally {
         setIsLoading(false);
       }
     };
-    fetchDetalsMovie();
+    fetchDetailsMovie();
   }, [movieId]);
 
   const { title, overview, vote_average } = movie;
-  // console.log('movie', movie);
 
-  const handelGoBack = () => {
-    window.history.back(goBack); 
+  const handleGoBack = () => {
+    navigate(goBack);
   };
-
 
   return (
     <section>
       {error && <p>Sorry, something went wrong</p>}
       {isLoading && <Loader />}
-      <ButtonBack onClick={handelGoBack}>Go Back</ButtonBack>
+      <ButtonBack onClick={handleGoBack}>Go Back</ButtonBack>
       <div>
         <img src={urlImage} alt={title}></img>
         <div>
@@ -71,7 +61,7 @@ const MovieDetails = () => {
             {title}
             <span>{date}</span>
           </h1>
-          <p>User Score:{(vote_average * 10).toFixed(0)} %</p>
+          <p>User Score: {(vote_average * 10).toFixed(0)}%</p>
           <h2>Overview</h2>
           <p>{overview}</p>
           <h3>Genres</h3>
@@ -94,3 +84,102 @@ const MovieDetails = () => {
 };
 
 export default MovieDetails;
+
+
+
+// import { useEffect, useState, useRef } from 'react';
+// import { getDetailMovie } from 'components/Api/Api';
+// import { Loader } from 'components/Loader/Loader';
+// import { Outlet, useParams, Link, useLocation, useNavigate } from 'react-router-dom';
+// import { ButtonBack } from './MoviesDetails.styled';
+
+// const MovieDetails = () => {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+//   const goBack = useRef(location.state?.from ?? '/');
+  
+
+//   const { movieId } = useParams();
+//   const [movie, setMovie] = useState([]);
+//   const [genres, setGenres] = useState([]);
+//   const [date, setDate] = useState('');
+//   const [urlImage, setUrlImage] = useState('');
+//   const [error, setError] = useState(null);
+//   const [isLoading, setIsLoading] = useState(false);
+  
+  
+  
+
+//   useEffect(() => {
+//     const fetchDetalsMovie = async () => {
+//       try {
+//         setIsLoading(true)
+//         const movie = await getDetailMovie(movieId);
+//         const genres = movie.genres;
+//         const date = movie.release_date.split('-')[0];
+//         const url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+//         if (movie === []) {
+//           setError(`Інформація по фільму відсутня`);
+//         } else {
+//           setGenres(genres);
+//           setMovie(movie);
+//           setDate(date);
+//           setUrlImage(url);
+//         }
+//         // setGenres(genres);
+//         // setMovie(movie);
+//         // setDate(date);
+//         // setUrlImage(url);
+//       } catch (error) {
+//         setError(error.message);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     };
+//     fetchDetalsMovie();
+//   }, [movieId]);
+
+//   const { title, overview, vote_average } = movie;
+//   // console.log('movie', movie);
+
+//   const handelGoBack = () => {
+//     navigate(goBack); 
+//   };
+
+
+//   return (
+//     <section>
+//       {error && <p>Sorry, something went wrong</p>}
+//       {isLoading && <Loader />}
+//       <ButtonBack onClick={handelGoBack}>Go Back</ButtonBack>
+//       <div>
+//         <img src={urlImage} alt={title}></img>
+//         <div>
+//           <h1>
+//             {title}
+//             <span>{date}</span>
+//           </h1>
+//           <p>User Score:{(vote_average * 10).toFixed(0)} %</p>
+//           <h2>Overview</h2>
+//           <p>{overview}</p>
+//           <h3>Genres</h3>
+//           <p>{genres.map(el => el.name).join(' / ')}</p>
+//         </div>
+//       </div>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="cast">Cast</Link>
+//           </li>
+//           <li>
+//             <Link to="reviews">Reviews</Link>
+//           </li>
+//         </ul>
+//         <Outlet />
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default MovieDetails;
